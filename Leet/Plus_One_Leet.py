9706fdae556dbc1516bdf44eadd73d65856b3cb5
MANISH-DESWAL <manishdeswal0899@gmail.com>
2026-01-12 23:57:39 +0530

    Remove Leet directory

--- Leet/Plus_One_Leet.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        #here we firstly converted list into str then int and added +1 
-        num=int("".join(map(str,digits))) + 1
-        return list(map(int,str(num))) #converted back to a list
-
-a=Solution()   
-print(a.plusOne([9,9,9,9])) 
