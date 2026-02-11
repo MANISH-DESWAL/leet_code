9e1da67ab5d19a4b83d76eab0d5128ce1327e2bb
MANISH-DESWAL <manishdeswal0899@gmail.com>
2026-02-11 20:53:28 +0530

    a new leetcode Problem

---
+++ Merge_LL_Sort.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        i=0
+        j=i+1
+        
+        
+
+        return lists
+
+
+a=Solution()
+print(a.mergeKLists([[1,2,3,4],[3,4,5,6]]))
+
+        
